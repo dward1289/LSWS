162decd3f24a31d910500a503fb8886039baa6a0
devona <dward1289@fullsail.edu>
2015-06-02 21:04:13 -0400

    Basics Added
    Frags added to main screen.BG and text added without custom font. Empty
    list view added.

---
+++ LosSantosWeeklySlammer/app/src/main/java/com/madgeek/devonaward/lossantosweeklyslammer/HomeList_Frag.java
@@ -0,0 +1,22 @@
+package com.madgeek.devonaward.lossantosweeklyslammer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by devonaward on 6/2/15.
+ */
+public class HomeList_Frag extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.listhomemenu_fragment,
+                container, false);
+        return view;
+    }
+}
